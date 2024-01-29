ac3e1d5284a02ca65262e0a35007ca269ea51116
David Grammatico <david.lugdunum@gmail.com>
2024-01-29 14:08:49 +0100

    implement todos-list

--- components/todos-list.tsx
+++ components/todos-list.tsx
@@ -1,1 +1,20 @@
 'use client';
+
+import { useTodos } from '@/hooks/use-todos';
+import TodoCard from './todo-card';
+
+export default function TodosList() {
+	const { todos } = useTodos({
+		initialTodos: [{ title: 'Hello World', completed: false, id: 1 }],
+	});
+	return (
+		<div className="w-full flex justify-center">
+			{todos.map((todo) => (
+				<TodoCard
+					key={todo.id}
+					todo={todo}
+				/>
+			))}
+		</div>
+	);
+}
